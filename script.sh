2d722ecbdef2bea8e0d4a18b1acf142864211f96
yusuke furukawa <littlefive.jp@gmail.com>
2015-10-06 04:50:10 +0900

    fix

--- script.sh
+++ script.sh
@@ -2,19 +2,9 @@
 
 sudo pacman -Syy --noconfirm extra/vim
 sudo pacman -Syy --noconfirm tmux
-#sudo pacman -Syy --noconfirm extra/python2-virtualenv
-#sudo pacman -Syy --noconfirm extra/python2-pip
 sudo pacman -Syy --noconfirm wget
 sudo pacman -Syy --noconfirm git
 sudo pacman -Syy --noconfirm tree
 sudo pacman -Syy --noconfirm xsel
-#sudo pip2.7 install virtualenvwrapper
 
-#cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-#cd ~ && ~/.dropbox-dist/dropboxd 
-
-#+ rcfile bash vimrc
-#+ vim bundle install 
-#+ ctags
-
-git clone https://github.com/tmux-plugins/tmux-yank ~/rcfiles
+#git clone https://github.com/frkwy/dotfiles.git
