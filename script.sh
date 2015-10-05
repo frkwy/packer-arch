71817276a4f2a043f1f73c0b80469200dd21fb65
yusuke furukawa <littlefive.jp@gmail.com>
2015-10-06 02:55:36 +0900

    packer

--- script.sh
+++ script.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
 sudo pacman -Syy --noconfirm extra/vim
-#+ vim
-#+ python
-#+ virtualenv
+sudo pacman -Syy --noconfirm tmux
+#sudo pacman -Syy --noconfirm extra/python2-virtualenv
+#sudo pacman -Syy --noconfirm extra/python2-pip
+sudo pacman -Syy --noconfirm wget
+sudo pacman -Syy --noconfirm git
+sudo pacman -Syy --noconfirm tree
+sudo pacman -Syy --noconfirm xsel
+#sudo pip2.7 install virtualenvwrapper
+
+#cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
+#cd ~ && ~/.dropbox-dist/dropboxd 
+
 #+ rcfile bash vimrc
 #+ vim bundle install 
-#+ tmux
 #+ ctags
-#
+
+git clone https://github.com/tmux-plugins/tmux-yank ~/rcfiles
