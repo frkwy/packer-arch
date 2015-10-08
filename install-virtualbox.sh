bedaaf4534f56b1a03d2c4e0c0c1354765348225
y-furukawa <y-furukawa@sakura.ad.jp>
2015-10-08 10:48:28 +0900

    fix

--- install-virtualbox.sh
+++ install-virtualbox.sh
@@ -32,6 +32,24 @@ echo "==> mounting ${ROOT_PARTITION} to ${TARGET_DIR}"
 
 echo "LOCALE=ja_JP.UTF-8" > /etc/rc.conf
 
+mkdir $HOME/.config
+echo "
+LANG=ja_JP.UTF-8
+LC_CTYPE=ja_JP.UTF-8
+LC_NUMERIC=ja_JP.UTF-8
+LC_TIME=ja_JP.UTF-8
+LC_COLLATE=ja_JP.UTF-8
+LC_MONETARY=ja_JP.UTF-8
+LC_MESSAGES=ja_JP.UTF-8
+LC_PAPER="ja_JP.UTF-8"
+LC_NAME="ja_JP.UTF-8"
+LC_ADDRESS="ja_JP.UTF-8"
+LC_TELEPHONE="ja_JP.UTF-8"
+LC_MEASUREMENT="ja_JP.UTF-8"
+LC_IDENTIFICATION="ja_JP.UTF-8"
+LC_ALL=
+" > $HOME/.config/locale.conf
+
 echo "==> settings mirror list to jp"
 echo "##
 ## Arch Linux repository mirrorlist
